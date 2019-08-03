509600e03b0a95a4191bb1b22381e900b617ae9d
Sunnylnwza007 <trinnakrit@gmail.com>
2019-08-03 14:11:49 +0700

    Q8

--- 59160090/Program.cs
+++ 59160090/Program.cs
@@ -45,9 +45,21 @@ namespace _59160090
                 // Q1  
             // Console.WriteLine(sum(salary));
                 // Q2
-            // showSalary(employee,salary);
+            // showSalary();
                 // Q3
-            showSalaryWithDeduct(employee,salary);
+            // showSalaryWithDeduct();
+                // Q4
+            //showSalaryWithDeductNet();
+                // Q5
+            //showSalaryWithDeductAndBonus();
+                // Q6
+            //showFund();
+                // Q7
+            //showFund2();
+                // Q8
+            showFund3();
+
+
         }
         // Q1
         static int sum (int[] x){
@@ -58,14 +70,14 @@ namespace _59160090
         }
 
         // Q2
-        static void showSalary(String[] employee,int[] salary){
+        static void showSalary(){
             for (int i=0;i<employee.Length;i++){
                 Console.WriteLine(employee[i]+" "+salary[i]);
             }
         }
 
         // Q3
-        static void showSalaryWithDeduct (String[] employee,int[] salary){
+        static void showSalaryWithDeduct (){
             for (int i =0;i<employee.Length;i++){
                 int deduct = 0;
                 if (salary[i]<15000){
@@ -77,7 +89,101 @@ namespace _59160090
             
             }
         }
-    }
+
+        // Q4
+        static void showSalaryWithDeductNet (){
+            int netDeduct = 0;
+            for (int i =0;i<employee.Length;i++){
+                int deduct = 0;
+                int deductCompany = 0;
+                if (salary[i]<15000){
+                    deduct = (salary[i]*5)/100;
+                }else{
+                    deduct = (15000*5)/100;
+                }
+                deductCompany = deduct;
+                Console.WriteLine("Name: "+employee[i]+" Salary: "+salary[i]+" Deduct: "+deduct+" CompanyDeduct: "+deduct);
+                netDeduct+= deduct+deductCompany;
+            }
+            Console.WriteLine("Net: "+netDeduct);
+        }
+
+        // Q5
+        static void showSalaryWithDeductAndBonus (){
+            for (int i =0;i<employee.Length;i++){
+                int Bonus = salary[i]/2;
+                int deduct = 0;
+                if (salary[i]<15000){
+                    deduct = (salary[i]*5)/100;
+                }else{
+                    deduct = (15000*5)/100;
+                }
+                int income = (salary[i]+Bonus)-deduct;
+            Console.WriteLine("Name: "+employee[i]+" Salary: "+salary[i]+" Deduct: "+deduct+" Bonus: "+Bonus+" Income: "+income);
+            }
+        }
+
+        // Q6
+        static void showFund(){
+            for (int i =0;i<employee.Length;i++){
+                int deduct = 0;
+                double empfund= 0;
+                double compfund = 0;
+                if (salary[i]<15000){
+                    deduct = (salary[i]*5)/100;
+                }else{
+                    deduct = (15000*5)/100;
+                }
+                int sumSalary = salary[i]-deduct;
+                empfund = sumSalary*0.05;
+                compfund = sumSalary*0.02;
+                
+                Console.WriteLine("Name: "+employee[i]+" Salary: "+sumSalary+" Empfund: "+empfund+" Comfund: "+compfund);
+        
+            }
+        }
+        // Q7
+        static void showFund2 (){
+            for (int i =0;i<employee.Length;i++){
+                int deduct = 0;
+                double empfund= 0;
+                if (salary[i]<15000){
+                    deduct = (salary[i]*5)/100;
+                }else{
+                    deduct = (15000*5)/100;
+                }
+                int sumSalary = salary[i]-deduct;
+                empfund = sumSalary*0.05;
+                double income = (double)sumSalary - empfund;
+                
+                Console.WriteLine("Name: "+employee[i]+" Salary: "+salary[i]+" Deduct: "+deduct+" Deduct2: "+empfund+" NetIncome: "+income);
+        
+            }
+        }
+
+        // Q8
+        static void showFund3 (){
+            for (int i =0;i<employee.Length;i++){
+                int deduct = 0;
+                double empfund= 0;
+                if (salary[i]<15000){
+                    deduct = (salary[i]*5)/100;
+                }else{
+                    deduct = (15000*5)/100;
+                }
+                int sumSalary = salary[i]-deduct;
+                double income = 0;
+                if (employee[i].Equals("A") || employee[i].Equals("C") ||employee[i].Equals("E")){
+                    income = (double) sumSalary;
+                }else{
+                    empfund = sumSalary*0.05;
+                    income = (double)sumSalary - empfund;
+                }
+                Console.WriteLine("Name: "+employee[i]+" Salary: "+salary[i]+" Deduct: "+deduct+" Deduct2: "+empfund+" NetIncome: "+income);
+        
+            }
+        }
 
     
+    }
 }
